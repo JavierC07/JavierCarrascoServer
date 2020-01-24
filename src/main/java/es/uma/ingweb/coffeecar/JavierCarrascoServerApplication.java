5180db6c280022f8470ad30509969a7fda4bf274
JavierC07 <36677101+JavierC07@users.noreply.github.com>
2020-01-24 13:50:16 +0100

    eso

--- src/main/java/es/uma/ingweb/coffeecar/JavierCarrascoServerApplication.java
+++ src/main/java/es/uma/ingweb/coffeecar/JavierCarrascoServerApplication.java
@@ -1,20 +1,14 @@
 package es.uma.ingweb.coffeecar;
-import es.uma.ingweb.coffeecar.entities.User;
-import lombok.extern.java.Log;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
-public class CoffeecarApplication {
+public class JavierCarrascoServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CoffeecarApplication.class, args);
+        SpringApplication.run(JavierCarrascoServerApplication.class, args);
     }
 
     @Bean
